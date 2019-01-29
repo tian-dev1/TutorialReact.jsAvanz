1639dc992bf68bf325130a4d6d4df588b3a42111
Cristian Camilo <tiancamilo59@gmail.com>
2019-01-29 11:31:20 -0500

    Empezamos con el segundo tutorial de react con la misma página del primer curso

---
+++ src/requests/places.js
@@ -0,0 +1,19 @@
+export default {
+  places: [
+    {
+      imageUrl: '/images/imagen1.png',
+      title: 'Desayunos el rey',
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+    },
+    {
+      imageUrl: '/images/imagen2.png',
+      title: 'Starbucks Norte',
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+    },
+    {
+      imageUrl: '/images/imagen3.png',
+      title: 'Pizza de amor',
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+    }
+  ]
+}
