b28a3c81d2ec8e9e3e4b7a38c9abaf5ca1427496
Cristian Camilo <tiancamilo59@gmail.com>
2019-01-30 15:06:04 -0500

    Se implemento un ejemplo de permisos y la forma de acceder a las rutas /new

--- src/Router.js
+++ src/Router.js
@@ -7,18 +7,36 @@ import {
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import App from './App';
 
+
+
+const UserSignedIn = true;
 export default class Router extends React.Component{
+//Rutas que se acceden con permiso 
+    
+    signedInRoutes(){
+        if(true){
+            return(
+                <Route path="/new" render={()=> <h1>Bienvenido</h1> } />
+            );
+        }
+    }
+
+    home(){
+        if(UserSignedIn) return Home;
+        return Dashboard;
+    }
 
     render(){
         return(
             <ReactRouter>
                 <App>
-                    <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/" component={this.home()}></Route>
                     <Route path="/login" component={Login}></Route> 
                     <Route path="/signup" component={Login}></Route> 
-                    
+                    {this.signedInRoutes()}
                 </App>
             </ReactRouter>
         );
